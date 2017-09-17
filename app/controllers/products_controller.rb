e416754101fe32b2071199c29b6825f2680f32e6
dimon-k <dmitriy.kanunnikov@gmail.com>
2017-09-17 19:16:20 +0200

    Add CRUD for Product model (Name and Price only)

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,40 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save ? (redirect_to products_path) : (render 'new')
+  end
+
+  def update
+    @product.update(product_params) ? (redirect_to products_path) : (render 'edit')
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
